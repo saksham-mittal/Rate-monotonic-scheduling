b21d9ba9a0e595533375286bf043099c2b88fe28
saksham-mittal <mittalsaksham01@gmail.com>
2018-01-21 16:49:15 +0530

    main code written

--- rate-monotonic-scheduling.cpp
+++ rate-monotonic-scheduling.cpp
@@ -5,12 +5,32 @@ using namespace std;
 
 class process {
 	public:
+		int m_id;		// Id of the process
 		int m_period;	// Time period of arrival of processes
 		int m_bursts;	// Processing time of process
 		// deadline of the processes is assumed to be same as m_period
 };
 
 queue <process> readyQueue;
+
+bool priority(const process &x, const process &y) {
+	return (x.m_period < y.m_period);
+}
+
+void sortQueue() {
+	vector<process> v;
+	while(!readyQueue.empty()) {   
+		v.push_back(*readyQueue.top());
+	    readyQueue.dequeue();
+	}
+
+	sort(v.begin(), v.end(), priority);
+
+	for(int i=0; i<v.size(); i++) {   
+		readyQueue.enqueue(v[i]);
+	}
+
+}
 
 int main(int argc, char const *argv[]) {
 	// Your code here
@@ -30,6 +50,7 @@ int main(int argc, char const *argv[]) {
 	cout << "Enter the details of the processes: " << endl;
 	for(int i=0; i<noOfProcesses; i++) {
 		cout << "Process " << i + 1 << ": " << endl;
+		P[i].m_id = i + 1;
 		cout << "Period: ";
 		cin >> P[i].m_period;
 		cout << "Processing time: ";
@@ -41,8 +62,12 @@ int main(int argc, char const *argv[]) {
 	for(int i=0; i<noOfProcesses; i++) {
 		readyQueue.push(P[i]);
 	}
+	// sort the ready queue
+	sortQueue();
 
-	while(time != t) {
+	process current = readyQueue.pop();
+	cout << "1 ";
+	while(time <= t) {
 		time++;
 		// check if a new process has to be added in the ready queue
 		for(int i=0; i<noOfProcesses; i++) {
@@ -50,7 +75,31 @@ int main(int argc, char const *argv[]) {
 				readyQueue.push(P[i]);
 			}
 		}
-		// currently the readyQueue.pop() process will excecute
+		// sort the ready queue
+		sortQueue();
+
+		// check if burst time is over, else continue excecuting
+		if(time == current.m_bursts) {
+			// Its processing time is over
+			cout << "P" << current.m_id << " " << time << endl;
+		} else {
+			// means current is excecuting
+			current.m_bursts--;
+		}
+
+		if(current.m_bursts == 0 && readyQueue.size() != 0) {
+			// If the current process has finished excecuting
+			// and ready queue is not empty
+			current = readyQueue.pop();
+			cout << time << " ";
+		} else if(readyQueue.front().m_period < current.m_period) {
+			// Preempt the process if another higher priority process is present in the ready queue
+			readyQueue.push(current);
+			// sort the ready queue
+			sortQueue();
+			current = readyQueue.pop();
+			cout << time << " ";
+		}
 
 	}
 	return 0;
